ee4196724f39d133dad7adf6f5de0696971a8341
Val_M <v.mischenko90@gmail.com>
2023-01-19 14:25:33 +0200

    add fornts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { CssBaseline, Typography, ThemeProvider } from '@mui/material';
+import { theme } from './global-styles/theme';
+import './global-styles/global-styles.scss';
+
 function App() {
-  return <div>working</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <div>
+          <Typography>Font Check</Typography>
+        </div>
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
 
 export default App;
