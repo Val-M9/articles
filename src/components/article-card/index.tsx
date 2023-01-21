a0a21fef9b6963b5104323101158a01d443ab724
Val_M <v.mischenko90@gmail.com>
2023-01-21 19:31:29 +0200

    add arcticle card component

---
+++ src/components/article-card/index.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { Box, Button, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { formatDate } from '../../helpers';
+import { ArrowRight, Calendar } from '../svg';
+import { ArticleDto } from '../../common/types';
+import styles from './styles.module.scss';
+
+const ArticleCard: FC<Partial<ArticleDto>> = ({ id, imageUrl, publishedAt, title, summary }) => {
+  let date = '--/--/----';
+
+  if (publishedAt) {
+    date = formatDate(publishedAt);
+  }
+
+  return (
+    <Grid item key={id}>
+      <Card className={styles.card}>
+        <CardMedia component="img" alt="poster" image={imageUrl} className={styles.picture} />
+        <CardContent className={styles.cardContent}>
+          <Box className={styles.date}>
+            <Calendar />
+            <Typography>{date}</Typography>
+          </Box>
+          <Typography variant="h3" className={styles.title}>
+            {title}
+          </Typography>
+          <Typography className={styles.description}>{summary}</Typography>
+          <Button className={styles.btn}>
+            Read more <ArrowRight />
+          </Button>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export { ArticleCard };
--- src/global-styles/theme.ts
+++ src/global-styles/theme.ts
@@ -3,5 +3,25 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   typography: {
     fontFamily: ['Montserrat', 'sans-serif'].join(','),
+    allVariants: {
+      color: '#363636',
+    },
+  },
+  components: {
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: '5px',
+          boxShadow: '0 8px 24px rgba(0, 0, 0, 0.05)',
+        },
+      },
+    },
+    MuiCardContent: {
+      styleOverrides: {
+        root: {
+          padding: 0,
+        },
+      },
+    },
   },
 });
