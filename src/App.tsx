0d28087c4f806203996f414b7056bf42e3ec688c
Val_M <v.mischenko90@gmail.com>
2023-01-30 03:20:12 +0200

    feature redux persist storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Provider } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router';
 import { theme } from './global-styles/theme';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
 import { AppRoutes } from './common/enums';
 import { ArticlePage, Articles } from './pages';
 import './global-styles/global-styles.scss';
@@ -11,14 +12,16 @@ import './global-styles/global-styles.scss';
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline>
-          <Routes>
-            <Route path={AppRoutes.ARTICLES} element={<Articles />} />
-            <Route path={AppRoutes.ARTICLE_INFO} element={<ArticlePage />} />
-          </Routes>
-        </CssBaseline>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <Routes>
+              <Route path={AppRoutes.ARTICLES} element={<Articles />} />
+              <Route path={AppRoutes.ARTICLE_INFO} element={<ArticlePage />} />
+            </Routes>
+          </CssBaseline>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,43 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { apiCall } from '../api/api-call';
 import { articlesReducer } from './reducer';
 
-export const extraArgument = {
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, articlesReducer);
+
+const extraArgument = {
   apiCall,
 };
 
 const store = configureStore({
-  reducer: { articles: articlesReducer },
+  reducer: { articles: persistedReducer },
   middleware: (getDefaultMiddleware) => {
     const defaultMiddleware = getDefaultMiddleware({
       thunk: { extraArgument },
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     });
 
     return defaultMiddleware;
   },
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor, extraArgument };
